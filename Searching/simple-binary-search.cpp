8588437a53e4aee30d0d4aaa895b0f6dcd49c5d5
devdattapandey2512 <devdattapandey.dp1@gmail.com>
2020-09-20 15:04:49 +0530

    binary search prog added

---
+++ Searching/simple-binary-search.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+
+        return binarySearch(arr, mid + 1, r, x);
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n, x;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> x;
+    cout << binarySearch(arr, 0, n - 1, x);
+
+    return 0;
+}
